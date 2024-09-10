43c2c1f3c7330724d7af800182d8fad15d41047e
ErickParra <39738722+ErickParra@users.noreply.github.com>
2024-09-10 11:19:53 -0300

    Update Streamlit_TTM.py

--- Streamlit_TTM.py
+++ Streamlit_TTM.py
@@ -101,10 +101,10 @@ df_pivot[columns_to_numeric] = df_pivot[columns_to_numeric].apply(pd.to_numeric,
 
 # Manejo de valores faltantes (Interpolación por tiempo y resampling)
 df_pivot.interpolate(method='time', inplace=True)
-df_resampled = df_pivot.resample('1T', on='ReadTime').mean()  # Resampling a intervalos de 1 minuto
-df_resampled.fillna(method='bfill', inplace=True)  # Rellenar valores faltantes hacia atrás
+#df_resampled = df_pivot.resample('1T', on='ReadTime').mean()  # Resampling a intervalos de 1 minuto
+#df_resampled.fillna(method='bfill', inplace=True)  # Rellenar valores faltantes hacia atrás
 
 # Obtener datos en tiempo real desde Databricks y preprocesarlos
 #df_cleaned = get_data_from_databricks()
 st.write("Datos Resampleados:")
-st.write(df_resampled)  # Esta línea mostrará el dataframe en la aplicación Streamlit
+st.write(df_pivot)  # Esta línea mostrará el dataframe en la aplicación Streamlit
