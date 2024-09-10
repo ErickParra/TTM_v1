1ed72e0381cda476de1da6827903040a08553be5
ErickParra <39738722+ErickParra@users.noreply.github.com>
2024-09-10 12:42:18 -0300

    Update Streamlit_TTM.py

--- Streamlit_TTM.py
+++ Streamlit_TTM.py
@@ -15,6 +15,8 @@ from tsfm_public.toolkit.time_series_preprocessor import TimeSeriesPreprocessor
 
 from tsfm_public import TinyTimeMixerForPrediction, TrackingCallback, count_parameters, load_dataset
 
+from tsfm_public.toolkit.visualization import visualization
+
 # Acceder a los secrets almacenados en Streamlit Cloud
 server = st.secrets["server"]
 http = st.secrets["http"]
