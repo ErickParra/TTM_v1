d1f8ac9ec249437889cc69cc4381a2d5081dc9ae
ErickParra <39738722+ErickParra@users.noreply.github.com>
2024-09-10 12:31:55 -0300

    Update Streamlit_TTM.py

--- Streamlit_TTM.py
+++ Streamlit_TTM.py
@@ -4,7 +4,7 @@ import pandas as pd
 from datetime import datetime, timedelta
 from databricks import sql
 import matplotlib.pyplot as plt
-from tsfm_public.toolkit.visualization import plot_predictions
+#from tsfm_public.toolkit.visualization import plot_predictions
 
 import streamlit as st
 import pandas as pd
