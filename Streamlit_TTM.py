3cb0c8f5d09df5e9cb2e8877c04d3bf0fcfd1b8a
ErickParra <39738722+ErickParra@users.noreply.github.com>
2024-09-10 12:32:47 -0300

    Update Streamlit_TTM.py

--- Streamlit_TTM.py
+++ Streamlit_TTM.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from transformers import TinyTimeMixerForPrediction
+#from transformers import TinyTimeMixerForPrediction
 from datetime import datetime, timedelta
 from databricks import sql
 import matplotlib.pyplot as plt
