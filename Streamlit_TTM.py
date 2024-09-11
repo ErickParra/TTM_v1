d4e616675701369c2fbe6e55ba727a4790288969
ErickParra <39738722+ErickParra@users.noreply.github.com>
2024-09-11 00:05:20 -0300

    Update Streamlit_TTM.py

--- Streamlit_TTM.py
+++ Streamlit_TTM.py
@@ -38,7 +38,7 @@ def get_data_from_databricks():
 
     # Obtener el tiempo actual y calcular las últimas 48 horas
     time_now = datetime.now()
-    time_48_hours_ago = time_now - timedelta(hours=90)
+    time_48_hours_ago = time_now - timedelta(hours=76)
 
     # Formatear las fechas para SQL
     time_now_str = time_now.strftime('%Y-%m-%d %H:%M:%S')
