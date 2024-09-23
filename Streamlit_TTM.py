8d34947ab08905a1621b910ee2d5a0285c156b70
ErickParra <39738722+ErickParra@users.noreply.github.com>
2024-09-23 09:40:22 -0300

    Update Streamlit_TTM.py

--- Streamlit_TTM.py
+++ Streamlit_TTM.py
@@ -47,7 +47,7 @@ def get_data_from_databricks():
     last_time = pd.to_datetime(last_time_df['last_time'].iloc[0])
 
     # Calcular las últimas 48 horas desde el último tiempo registrado
-    time_48_hours_ago = last_time - timedelta(hours=50)
+    time_48_hours_ago = last_time - timedelta(hours=48)
 
     # Formatear las fechas para SQL
     last_time_str = last_time.strftime('%Y-%m-%d %H:%M:%S')
